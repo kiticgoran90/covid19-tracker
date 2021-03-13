de7c4a91d19c565e9f58584211d2180b67087574
Goran Kitic <kiticgoran90@gmail.com>
2021-03-13 14:47:17 +0100

    Edited table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -6,14 +6,18 @@ import numeral from 'numeral'
 function Table({ countries }) {
     return (
         <div className="table">
-            {
-                countries.map((country, i ) => (
-                    <tr key={i}>
-                        <td>{country.country}</td>
-                        <td><strong>{numeral(country.cases).format("0,0")}</strong></td>
-                    </tr>
-                ))
-            }
+            <table>
+                {
+                    countries.map((country, i ) => (
+                        <tbody key={i}>
+                            <tr>
+                                <td>{country.country}</td>
+                                <td><strong>{numeral(country.cases).format("0,0")}</strong></td>
+                            </tr>
+                        </tbody>
+                    ))
+                }
+            </table>
         </div>
     )
 }
